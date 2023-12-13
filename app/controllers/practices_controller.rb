0e8fb0bfd5275ff1ca1fdb9e255b04ce87bfeabb
Shunmurama <mura.faily72@gmail.com>
2023-12-13 06:42:45 +0000

    first commit

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -7,9 +7,23 @@ class PracticesController < ApplicationController
   def create
     @practice = Practice.new(practice_params)
     @practice.save
+    redirect_to practices_path
   end
 
   def edit
+    @practice = Practice.find(params[:id])
+  end
+
+  def update
+    @practice = Practice.find(params[:id])
+    @practice.update(practice_params)
+    redirect_to practices_path
+  end
+
+  def destroy
+    practice = Practice.find(params[:id])
+    practice.destroy
+    redirect_to practices_path
   end
 
   private
